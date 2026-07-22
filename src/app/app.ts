8f81a35c82b6fdab9728cc5a117d1cae7880e15e
AlphaTanmoy <dtanmoy169@gmail.com>
2026-07-22 21:12:06 +0530

    landing page done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar';
 import { HeroComponent } from './shared/components/hero/hero';
+import { FooterComponent } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/features/home/pages/landing/landing.ts
+++ src/app/features/home/pages/landing/landing.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../../shared/components/navbar/navbar';
-import { Footer } from '../../../../shared/components/footer/footer';
+import { FooterComponent } from '../../../../shared/components/footer/footer';
 import { HeroComponent } from '../../../../shared/components/hero/hero';
 import { Features } from '../../../../shared/components/features/features';
 import { Models } from '../../../../shared/components/models/models';
@@ -8,7 +8,7 @@ import { Faq } from '../../../../shared/components/faq/faq';
 
 @Component({
   selector: 'app-landing',
-  imports: [NavbarComponent,Footer,HeroComponent],
+  imports: [NavbarComponent,FooterComponent,HeroComponent],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
