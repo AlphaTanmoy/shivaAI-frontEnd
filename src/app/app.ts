bbe9e3c5abf259bc6cb832d1339695891aa1189d
AlphaTanmoy <dtanmoy169@gmail.com>
2026-07-22 22:06:53 +0530

    bug fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './shared/components/navbar/navbar';
-import { FooterComponent } from './shared/components/footer/footer';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
